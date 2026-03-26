4e1c0c8b9f630a3c648eb18e79b33b08f5abe0c1
hhfrtvvccv-prog <hhfrtvvccv@gmail.com>
2026-03-26 23:54:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
+
 client.on('qr', qr => {
-    console.log('امسح الكود ده بسرعة:');
     qrcode.generate(qr, {small: true});
+    console.log('امسح الكود ده بالواتساب حالاً:');
 });
-client.on('ready', () => console.log('البوت جاهز يا فرعون!'));
-client.on('message', msg => {
-    if (msg.body === 'هلا') msg.reply('هلا والله، السيرفر شغال والبوت معاك!');
+
+client.on('ready', () => {
+    console.log('البوت شغال وزي الفل!');
 });
+
 client.initialize();
